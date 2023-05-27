c2785dcae05ff3b427774de37c7ea26c32484454
mut4bor <trhorse2004@gmail.com>
2023-05-27 21:17:44 +0300

    serious cart changings

--- dist/script.js
+++ dist/script.js
@@ -1,126 +1,120 @@
-//* Functions
-
-const StartBlockSwiper = new Swiper('.start-block__swiper', {
+const StartBlockSwiper = new Swiper(".start-block__swiper", {
   navigation: {
-    nextEl: '.start-block__swiper-button-next',
-    prevEl: '.start-block__swiper-button-prev',
+    nextEl: ".start-block__swiper-button-next",
+    prevEl: ".start-block__swiper-button-prev",
   },
-	pagination: {
-		el: '.start-block__swiper-pagination',
-		clickable: true,
-		type: 'bullets',
-	},
-	keyboard: {
-		enabled: true,
-		onlyInViewport: true
-	},
-	autoplay: {
-		delay: 5000,
-		disableOnInteraction: false, 
-	},
-	speed: 500,
-	simulateTouch: false,
-	direction: 'horizontal',
+  pagination: {
+    el: ".start-block__swiper-pagination",
+    clickable: true,
+    type: "bullets",
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  speed: 500,
+  simulateTouch: false,
+  direction: "horizontal",
   loop: true,
-	loopedSlides: 1,
-	slidesPerView: 1,
-	watchOverflow: true,
-	spaceBetween: 30,
-	slidesPerGroup: 1,
-	centeredSlides: true,
-	initialSlide: 0,
+  loopedSlides: 1,
+  slidesPerView: 1,
+  watchOverflow: true,
+  spaceBetween: 30,
+  slidesPerGroup: 1,
+  centeredSlides: true,
+  initialSlide: 0,
 });
 
-const ReviewBlockSwiper = new Swiper('.review-block__swiper', {
+const ReviewBlockSwiper = new Swiper(".review-block__swiper", {
   navigation: {
-    nextEl: '.review-block__swiper-button-next',
-    prevEl: '.review-block__swiper-button-prev',
+    nextEl: ".review-block__swiper-button-next",
+    prevEl: ".review-block__swiper-button-prev",
   },
-	// pagination: {
-	// 	el: '.review-block__swiper-pagination',
-	// 	clickable: true,
-	// 	type: 'bullets',
-	// },
-	keyboard: {
-		enabled: true,
-		onlyInViewport: true
-	},
-	breakpoints: {
-		320: {
-			slidesPerView: 1
-		},
-		768: {
-			slidesPerView: 2
-		},
-		1024: {
-			slidesPerView: 3
-		},
-	},
-	// autoplay: {
-	// 	delay: 5000,
-	// 	disableOnInteraction: false, 
-	// },
-	speed: 500,
-	simulateTouch: true,
-	direction: 'horizontal',
-	// autoHeight: 'true',
+  // pagination: {
+  // 	el: '.review-block__swiper-pagination',
+  // 	clickable: true,
+  // 	type: 'bullets',
+  // },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+  // autoplay: {
+  // 	delay: 5000,
+  // 	disableOnInteraction: false,
+  // },
+  speed: 500,
+  simulateTouch: true,
+  direction: "horizontal",
+  // autoHeight: 'true',
   // loop: true,
-	// loopedSlides: 3,
-	slidesPerView: 3,
-	watchOverflow: true,
-	spaceBetween: 20,
-	// slidesPerGroup: 1,
-	centeredSlides: false
+  // loopedSlides: 3,
+  slidesPerView: 3,
+  watchOverflow: true,
+  spaceBetween: 20,
+  // slidesPerGroup: 1,
+  centeredSlides: false,
 });
 
-const OtherCardSwiper = new Swiper('.other-card', {
+const OtherCardSwiper = new Swiper(".other-card", {
   navigation: {
-    nextEl: '.other-card__swiper-button-next',
-    prevEl: '.other-card__swiper-button-prev',
+    nextEl: ".other-card__swiper-button-next",
+    prevEl: ".other-card__swiper-button-prev",
   },
-	keyboard: {
-		enabled: true,
-		onlyInViewport: true
-	},
-	slidesPerView: 'auto',
-	speed: 200,
-	spaceBetween: 10,
-	simulateTouch: true,
-	direction: 'horizontal',
-	watchOverflow: true,
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+  slidesPerView: "auto",
+  speed: 200,
+  spaceBetween: 10,
+  simulateTouch: true,
+  direction: "horizontal",
+  watchOverflow: true,
 });
 
-const OtherCardImageSwiper = new Swiper('.card__image', {
+const OtherCardImageSwiper = new Swiper(".card__image", {
   navigation: {
-    nextEl: '.card__image-swiper-button-next',
-    prevEl: '.card__image-swiper-button-prev',
+    nextEl: ".card__image-swiper-button-next",
+    prevEl: ".card__image-swiper-button-prev",
   },
-	pagination: {
-		el: '.card__image-pagination',
-		clickable: true,
-	},
-	keyboard: {
-		enabled: true,
-		onlyInViewport: true
-	},
-	slidesPerView: 1,
-	speed: 200,
-	spaceBetween: -1,
-	simulateTouch: true,
-	direction: 'horizontal',
-	watchOverflow: true,
+  pagination: {
+    el: ".card__image-pagination",
+    clickable: true,
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+  slidesPerView: 1,
+  speed: 200,
+  spaceBetween: -1,
+  simulateTouch: true,
+  direction: "horizontal",
+  watchOverflow: true,
 });
 
-
-
-
-const InfoCardShowMore = document.querySelector('.info-card__show-more')
-InfoCardShowMore.addEventListener('click', function(){
-	document.querySelector('.info-card__content').classList.add('active');
-	InfoCardShowMore.classList.add('active');
-	document.querySelector('.detailed-info').classList.add('active');
+const InfoCardShowMore = document.querySelector(".info-card__show-more");
+InfoCardShowMore.addEventListener("click", function () {
+  document.querySelector(".info-card__content").classList.add("active");
+  InfoCardShowMore.classList.add("active");
+  document.querySelector(".detailed-info").classList.add("active");
 });
-
 
 function debounce(func, wait, immediate) {
   var timeout;
@@ -189,10 +183,12 @@ function linkChanger() {
     }
   });
 }
-
-window.addEventListener("scroll", () => {
-  debounce(linkChanger(), 300);
-});
+debounce(
+  window.addEventListener("scroll", () => {
+    linkChanger();
+  }),
+  300
+);
 
 let btn = document.querySelector(".show-more-button");
 let services = document.querySelector(".services-block");
@@ -205,15 +201,14 @@ function showMore() {
   if (btn.classList.contains("active")) {
     btn.textContent = "Скрыть";
 
-		document.querySelectorAll('.services-block__item').forEach(function(e){
-			e.classList.add('active')
-		});
-
+    document.querySelectorAll(".services-block__item").forEach(function (e) {
+      e.classList.add("active");
+    });
   } else {
     btn.textContent = "Показать больше";
-		document.querySelectorAll('.services-block__item').forEach(function(e){
-			e.classList.remove('active')
-		});
+    document.querySelectorAll(".services-block__item").forEach(function (e) {
+      e.classList.remove("active");
+    });
   }
 }
 
--- dist/script_cart.js
+++ dist/script_cart.js
@@ -1,110 +1,160 @@
+let checkbox = document.querySelectorAll(".checkbox");
+let basketDeliveryShippingInputsDivs = document.querySelectorAll(
+  ".basket-delivery__shipping-inputs div"
+);
+let basketDeliveryAdressLabel = document.querySelector('.basket-delivery__adress label');
+let basketDeliveryDateLabel = document.querySelector('.basket-delivery__date label');
+let basketDeliveryAdressInput = document.querySelector('#adress')
+let checkboxShipping = document.querySelector(".basket-delivery__shipping");
+// document.querySelector('.basket-delivery__yourself').checked = true
+checkboxShipping.checked = true;
 
-
-	document.querySelectorAll('.basket-content__text').forEach(function(e){
-		e.querySelector('a').classList.add('basket-content__a')
-		e.querySelector('p').classList.add('basket-content__p')
-	})
-
-	document.querySelectorAll('.price p').forEach(function(e) {
-		e.innerHTML = '<p>' + e.innerHTML + '</p>'+ '<p>руб</p>';
-	});
-
-
-	function data() {
-		const priceBasic = document.querySelectorAll('.price__basic')
-		priceBasic.forEach(function(e){
-			e.dataset.price = parseInt(e.innerText)
-		})
-	}
-
-	function priceCounter() {
-		
-		const cartItems = document.querySelectorAll('.basket-content__elements');
-		let totalPrice = 0
-		cartItems.forEach(function(e) {
-			const amount = e.querySelector('[data-counter]')
-			const price = e.querySelector('.price__basic')
-			const currentPrice = parseInt(amount.innerText) * parseInt(price.dataset.price)
-			totalPrice += currentPrice
-		})
-		document.querySelector('.basket-price__total div').innerHTML = '<h3>' + totalPrice + '</h3>' + '<p>руб</p>'
-	}
-
-
-	let toggleBtn, counter, counterWrapper, addRemove, tgglBtn, toCart, headlinesParent, headlineChildren, menuContent, menuList;
-
-window.addEventListener('click', function (e) {
-
-
-    if (e.target.classList.contains('add-to-cart__to-cart')) {
-        e.target.classList.toggle('active');
-        toggleBtn = e.target.closest('.add-to-cart');
-        addRemove = toggleBtn.querySelector('.add-to-cart__plus-minus');
-        addRemove.classList.toggle('active');
+function basketAdressProp() {
+  basketDeliveryShippingInputsDivs.forEach(function (element) {
+    if (checkboxShipping.checked == true) {
+      element.classList.remove("active");
+			basketDeliveryAdressLabel.innerText = 'Адрес доставки';
+			basketDeliveryDateLabel.innerText = 'Дата доставки';
+			basketDeliveryAdressInput.removeAttribute('disabled', 'disabled');
+			basketDeliveryAdressInput.value = ''
+    } else {
+			element.classList.add("active");
+			basketDeliveryAdressLabel.innerText = 'Адрес самовывоза';
+			basketDeliveryDateLabel.innerText = 'Дата самовывоза';
+			basketDeliveryAdressInput.setAttribute('disabled', 'disabled');
+			basketDeliveryAdressInput.value = 'Витебский проспект дом 47 корпус 4'
     }
+  });
+}
+basketAdressProp();
 
-
-    if (e.target.dataset.action === 'plus' || e.target.dataset.action === 'minus') {
-        counterWrapper = e.target.closest('.plus-minus');
-        counter = counterWrapper.querySelector('[data-counter]');
+checkbox.forEach(function (element) {
+  element.addEventListener("click", function () {
+    element.checked = true;
+    if (element.checked) {
+      checkbox.forEach(function (e) {
+        e.checked = false;
+        element.checked = true;
+      });
     }
-		
-		let basicPrice = e.target.closest('.basket-content__elements').querySelector('.price__basic')
-		
-    if (e.target.dataset.action === 'plus') {
-        counter.innerText = ++counter.innerText;
-				
-				basicPrice.innerHTML = basicPrice.dataset.price * parseInt(counter.innerText)+ '<p>руб</p>'
-				priceCounter();
-    }
-
-    if (e.target.dataset.action === 'minus') {
-        if (+counter.innerText > 1) {
-            counter.innerText = --counter.innerText;
-        }
-
-        else if (+counter.innerText == 1 && e.target.closest('.plus-minus').classList.contains('add-to-cart__plus-minus') == true) {
-            counterWrapper.classList.toggle('active');
-            tgglBtn = e.target.closest('.add-to-cart');
-            toCart = tgglBtn.querySelector('.add-to-cart__to-cart');
-            toCart.classList.toggle('active')
-        }
-				basicPrice.innerHTML = basicPrice.dataset.price * parseInt(counter.innerText)+ '<p>руб</p>'
-				priceCounter();
-    }
-
-    if (e.target.hasAttribute('data-headline') === true) {
-        headlinesParent = e.target.closest('.menu__headlines');
-        headlineChildren = headlinesParent.querySelectorAll('div');
-        headlineChild = headlinesParent.querySelector('.active');
-        menuContent = document.querySelector('.menu__content');
-        menuList = menuContent.querySelectorAll('.menu__list');
-
-        menuList.forEach(function (e, i) {
-            e.setAttribute('data-menuList', i + 1);
-        });
-
-        headlineChildren.forEach(function (e, i) {
-            e.classList.remove('active');
-            e.setAttribute('data-headline', i + 1)
-        });
-
-        e.target.classList.add('active');
-
-        dataHeadline = e.target.getAttribute('data-headline');
-        menuContainer = e.target.closest('.menu__container');
-        menuContent = menuContainer.querySelector('.menu__content');
-        menuList = menuContent.querySelectorAll('.menu__list');
-
-        menuList.forEach(function (e) {
-            e.classList.remove('active')
-            if (e.getAttribute('data-menuList') == dataHeadline) {
-                e.classList.add('active');
-            }
-        });
-
-    }
+    basketAdressProp();
+  });
 });
 
-	data()
-	priceCounter()
+document.querySelectorAll(".price p").forEach(function (e) {
+  e.innerHTML = "<p>" + e.innerHTML + "</p>" + "<p>руб</p>";
+});
+
+function data() {
+  const priceBasic = document.querySelectorAll(".price__basic");
+  priceBasic.forEach(function (e) {
+    e.dataset.price = parseInt(e.innerText);
+  });
+}
+
+function priceCounter() {
+  const cartItems = document.querySelectorAll(".basket-content__elements");
+  let totalPrice = 0;
+  cartItems.forEach(function (e) {
+    const amount = e.querySelector("[data-counter]");
+    const price = e.querySelector(".price__basic");
+    const currentPrice =
+      parseInt(amount.innerText) * parseInt(price.dataset.price);
+    totalPrice += currentPrice;
+  });
+  document.querySelector(".basket-price__total div").innerHTML =
+    "<h3>" + totalPrice + "</h3>" + "<p>руб</p>";
+}
+
+let toggleBtn,
+  counter,
+  counterWrapper,
+  addRemove,
+  tgglBtn,
+  toCart,
+  headlinesParent,
+  headlineChildren,
+  menuContent,
+  menuList;
+
+window.addEventListener("click", function (e) {
+  if (e.target.classList.contains("add-to-cart__to-cart")) {
+    e.target.classList.toggle("active");
+    toggleBtn = e.target.closest(".add-to-cart");
+    addRemove = toggleBtn.querySelector(".add-to-cart__plus-minus");
+    addRemove.classList.toggle("active");
+  }
+
+  if (
+    e.target.dataset.action === "plus" ||
+    e.target.dataset.action === "minus"
+  ) {
+    counterWrapper = e.target.closest(".plus-minus");
+    counter = counterWrapper.querySelector("[data-counter]");
+  }
+
+  let basicPrice = e.target
+    .closest(".basket-content__elements")
+    .querySelector(".price__basic");
+
+  if (e.target.dataset.action === "plus") {
+    counter.innerText = ++counter.innerText;
+
+    basicPrice.innerHTML =
+      basicPrice.dataset.price * parseInt(counter.innerText) + "<p>руб</p>";
+    priceCounter();
+  }
+
+  if (e.target.dataset.action === "minus") {
+    if (+counter.innerText > 1) {
+      counter.innerText = --counter.innerText;
+    } else if (
+      +counter.innerText == 1 &&
+      e.target
+        .closest(".plus-minus")
+        .classList.contains("add-to-cart__plus-minus") == true
+    ) {
+      counterWrapper.classList.toggle("active");
+      tgglBtn = e.target.closest(".add-to-cart");
+      toCart = tgglBtn.querySelector(".add-to-cart__to-cart");
+      toCart.classList.toggle("active");
+    }
+    basicPrice.innerHTML =
+      basicPrice.dataset.price * parseInt(counter.innerText) + "<p>руб</p>";
+    priceCounter();
+  }
+
+  if (e.target.hasAttribute("data-headline") === true) {
+    headlinesParent = e.target.closest(".menu__headlines");
+    headlineChildren = headlinesParent.querySelectorAll("div");
+    headlineChild = headlinesParent.querySelector(".active");
+    menuContent = document.querySelector(".menu__content");
+    menuList = menuContent.querySelectorAll(".menu__list");
+
+    menuList.forEach(function (e, i) {
+      e.setAttribute("data-menuList", i + 1);
+    });
+
+    headlineChildren.forEach(function (e, i) {
+      e.classList.remove("active");
+      e.setAttribute("data-headline", i + 1);
+    });
+
+    e.target.classList.add("active");
+
+    dataHeadline = e.target.getAttribute("data-headline");
+    menuContainer = e.target.closest(".menu__container");
+    menuContent = menuContainer.querySelector(".menu__content");
+    menuList = menuContent.querySelectorAll(".menu__list");
+
+    menuList.forEach(function (e) {
+      e.classList.remove("active");
+      if (e.getAttribute("data-menuList") == dataHeadline) {
+        e.classList.add("active");
+      }
+    });
+  }
+});
+
+data();
+priceCounter();
